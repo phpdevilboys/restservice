563e1165b02e468398fcf565831e9d780eaa8539
root <root@cdn79.(none)>
2013-07-12 13:07:48 +0530

    New Log Folder Added

---
+++ logs/log-2013-07-10.php
@@ -0,0 +1,11 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> log:: Object Initiated Successfully 
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> exceptionhook:: Object Initiated Successfully 
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> common file included
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> DB Object Initialized!!
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> restService:: Object Initiated Successfully 
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> Action Set:=>users
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> Action Method Set:=>Getuser
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> users:: Object Initiated Successfully 
+DEBUG - Wed Jul 10 12:17:13 IST 2013 --> Object of Class users created successfully
